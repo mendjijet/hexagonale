8d31e755f32fb22c17663194abd9cc4767f8f62c
mendjijet <mendjijet@gmail.com>
2024-03-17 08:32:26 +0100

    first commit for init project

---
+++ src/main/java/com/jet/hexagonale/HexagonaleApplication.java
@@ -0,0 +1,13 @@
+package com.jet.hexagonale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexagonaleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jet/hexagonale/HexagonaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jet.hexagonale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
